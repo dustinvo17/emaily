9cbb3392ef0ad380526d72b0c69f685da64d7324
dustinvo17 <46255218+dustinvo17@users.noreply.github.com>
2019-05-28 23:06:01 -0500

    deploy

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/',(req,res)=>{
+    res.send({hi:'there'})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,() =>{
+    console.log('app running on 5000')
+})
