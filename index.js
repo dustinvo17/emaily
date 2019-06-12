9b5f93c83404e2ec27d352b31243408ef31733a5
dustinvo17 <46255218+dustinvo17@users.noreply.github.com>
2019-06-12 17:02:41 -0500

    finish authentication

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const keys = require('./config/keys')
+const authRoutes = require('./routes/authRoutes')
 
 
-app.get('/',(req,res)=>{
-    res.send({bye:'buddy'})
-})
+require('./models/User')
+app.use(cookieSession({
+    maxAge: 30*24*60*60*1000 ,// 30 days
+    keys:[keys.cookieKey]
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+mongoose.connect(keys.mongoURI,{ useNewUrlParser: true })
+require('./services/passport')
 
 
 const PORT = process.env.PORT || 5000
+
+authRoutes(app)
 app.listen(PORT,() =>{
     console.log('app running on 5000')
 })
