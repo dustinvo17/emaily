8f4377d3f436c1f45ea81e092d59a10a00b423d9
dustinvo17 <46255218+dustinvo17@users.noreply.github.com>
2019-06-18 16:24:58 -0500

    prefix client render

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,6 @@
 const passport = require('passport')
 
 module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.send({
-            bye: 'buddy'
-        })
-    })
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['profile', 'email'],
         prompt: 'select_account'
